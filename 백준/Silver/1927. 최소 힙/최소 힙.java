bbfb6249c28fddf5d0cf60987104124511840862
ryudain <75834815+ryudain05@users.noreply.github.com>
2023-09-07 11:30:30 +0900

    [Silver II] Title: 최소 힙, Time: 1144 ms, Memory: 33612 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int input;
+
+        //int형 priorityQueue 선언 (내림차순)
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            input = Integer.parseInt(br.readLine());
+
+            //입력값이 0이면서 큐가 비어있으면 0반환, 그렇지 않으면 최소힙으로 정렬된 첫번째 값 반환
+            if (input == 0) {
+                if (q.isEmpty()) System.out.println(0);
+                else System.out.println(q.poll());
+            }
+            //그 외 모든 입력값은 큐에 추가
+            else q.offer(input);
+        }
+    }
+}
