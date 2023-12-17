b00647d517cde433995da19a14534f04eb1474da
ryudain <75834815+ryudain05@users.noreply.github.com>
2023-12-17 13:35:50 +0900

    [Silver IV] Title: 로프, Time: 424 ms, Memory: 26576 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Integer[] arr = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        int res = 0;
+
+        for (int i = 0; i < N; i++) {
+            res = Math.max(res, arr[i] * (i + 1));
+        }
+
+        System.out.println(res);
+
+    }
+}
