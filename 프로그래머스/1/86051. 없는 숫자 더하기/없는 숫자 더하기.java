94123b52d4910b6bea72f54e9af3b34faf4a056e
ryudain <75834815+ryudain05@users.noreply.github.com>
2024-09-16 23:05:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        for(int i: numbers) {
+            sum -= i;
+        }
+        
+        return sum;
+    }
+}
