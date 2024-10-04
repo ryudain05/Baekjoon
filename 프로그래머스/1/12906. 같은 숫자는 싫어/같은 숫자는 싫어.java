d2b006c7b93992bc50215d0d771098ef8c09bed0
ryudain <75834815+ryudain05@users.noreply.github.com>
2024-10-04 23:49:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.02 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+		
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        
+        for (int i=1; i<arr.length; i++){
+        	if (stack.peek() == arr[i]) {
+        		continue;
+        	} else {
+        		stack.push(arr[i]);
+        	}
+        }
+        
+
+        answer = new int[stack.size()];
+        for (int i=stack.size()-1; i>=0; i--) {
+        	answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
